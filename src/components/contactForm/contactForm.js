7aa4d13a8f465d4ba649e414dd1fcf9af2ad333f
milecki <m.kulecki@gmail.com>
2019-10-17 00:21:55 +0200

    Add CSS to Contact page, refactor buttons

--- src/components/contactForm/contactForm.js
+++ src/components/contactForm/contactForm.js
@@ -4,26 +4,26 @@ import styles from "./contactForm.module.scss"
 
 const ContactForm = () => (
   <div className={styles.formContainer}>
-    <h3 className={styles.h3}>Formularz kontaktowy</h3>
+    <h3 className={styles.formHeading}>Formularz kontaktowy</h3>
     <form className={styles.form}>
-      <label>
-        <input type="text" />
-        Imię i nazwisko
+      <label className={styles.label}>
+        <input type="text" className={styles.input} />
+        <span>Imię i nazwisko</span>
       </label>
 
-      <label>
-        <input type="text" />
-        Telefon
+      <label className={styles.label}>
+        <input type="text" className={styles.input} />
+        <span>Telefon</span>
       </label>
 
-      <label>
-        <input type="text" />
-        Email
+      <label className={styles.label}>
+        <input type="text" className={styles.input} />
+        <span>Email</span>
       </label>
 
-      <label>
-        <textarea name="" id="" cols="30" rows="10" />
-        Treść wiadomości
+      <label className={styles.label}>
+        <textarea className={styles.textarea} />
+        <span>Treść wiadomości</span>
       </label>
 
       <Button address="/">Wyślij wiadomość</Button>
--- src/components/contactInfo/contactInfo.js
+++ src/components/contactInfo/contactInfo.js
@@ -9,51 +9,50 @@ import { FaRegEnvelope } from "react-icons/fa"
 const ContactInfo = () => (
   <div className={styles.infoContainer}>
     <h3 className={styles.companyName}>
-      Profesor Szydło i Współpracownicy
-      <span>kancelaria prawna</span>
+      Profesor Szydło i Współpracownicy <span>kancelaria prawna</span>
     </h3>
     <p className={styles.address}>
       <FaMapMarkerAlt />
-      ul. Jaźwińska 5<span>54-060 Wrocław</span>
+      ul. Jaźwińska 5<br />
+      54-060 Wrocław
     </p>
     <p className={styles.email}>
       <FaRegEnvelope /> kancelaria@kancelariaszydlo.pl
     </p>
 
-    <div>
-      <div>
-        <p>
-          <FaUserCircle /> adw. dr Wojciech Szydło
-        </p>
-        <p>
-          <FaMobileAlt />
-          +48 602 108 636
-        </p>
-        <p>
-          <FaRegEnvelope /> wojciech.szydlo@kancelariaszydlo.pl
-        </p>
-      </div>
-      <div>
-        <p>
-          <FaUserCircle /> r.pr. dr Adam Haręża
-        </p>
-        <p>
-          <FaMobileAlt /> +48 513 969 696
-        </p>
-        <p>
-          <FaRegEnvelope /> adam.hareza@kancelariaszydlo.pl
-        </p>
-      </div>
+    <div className={styles.personContactInfo}>
+      <p>
+        <FaUserCircle /> adw. dr Wojciech Szydło
+      </p>
+      <p>
+        <FaMobileAlt />
+        +48 602 108 636
+      </p>
+      <p>
+        <FaRegEnvelope /> wojciech.szydlo@kancelariaszydlo.pl
+      </p>
+    </div>
 
-      <div>
-        <p>KRS: 0000321994</p>
-        <p>REGON: 020908422</p>
-      </div>
+    <div className={styles.personContactInfo}>
+      <p>
+        <FaUserCircle /> r.pr. dr Adam Haręża
+      </p>
+      <p>
+        <FaMobileAlt /> +48 513 969 696
+      </p>
+      <p>
+        <FaRegEnvelope /> adam.hareza@kancelariaszydlo.pl
+      </p>
+    </div>
 
-      <div>
-        <p>NIP: 8982151821</p>
-        <p>PKO BP SA, 38 1020 5242 0000 2502 0233 2773</p>
-      </div>
+    <div className={styles.registers}>
+      <p>KRS: 0000321994</p>
+      <p>REGON: 020908422</p>
+    </div>
+
+    <div className={styles.registers}>
+      <p>NIP: 8982151821</p>
+      <p>PKO BP SA, 38 1020 5242 0000 2502 0233 2773</p>
     </div>
   </div>
 )
